29525ab9751f82f99afd9de6a73cf62d8ab9e6e9
LawrenShin <simplyforgit@gmail.com>
2018-11-02 15:24:45 +0300

    get, not post

--- src/components/Users.js
+++ src/components/Users.js
@@ -3,29 +3,26 @@ import $ from 'jquery';
 
 class Users extends Component {
   render(){
-  //   const usersList = () => {
-  //     return $.ajax({
-  //       url: `https://front-test.now.sh/users?token=18476dc1c4fb11f4eebd2c4aaacdb3c14b3cd1e945dd8bc8456b73c8d4ef33cf`,
-  //       method: 'POST',
-  //       format: 'json'
-  //     });
-  //   };
+    const usersList = () => {
+      return $.ajax({
+        url: `https://front-test.now.sh/users?token=18476dc1c4fb11f4eebd2c4aaacdb3c14b3cd1e945dd8bc8456b73c8d4ef33cf`,
+        method: 'GET',
+        format: 'json'
+      });
+    };
 
-  //   const promiseUsersList = usersList();
-  //   promiseUsersList.fail((err) => {
-  //     console.log(err);
-  //   });
-  //   promiseUsersList.done((usersList) => {
-  //     console.log(usersList);
-  //   });
+    const promiseUsersList = usersList();
 
-  //  $.get( "https://front-test.now.sh/users?token=18476dc1c4fb11f4eebd2c4aaacdb3c14b3cd1e945dd8bc8456b73c8d4ef33cf" )
-  // .done(function(r) {
-  //   const usersList = r;
-  // })
-  // .fail(function(e) {
-  //   console.log(e);
-  // });
+    promiseUsersList.fail((err) => {
+      console.log(err);
+    });
+    promiseUsersList.done((res) => {
+      representData(res.data);
+    });
+
+    const representData = (usersList) => {
+      console.log(usersList);
+    }
 
     return(
       <div>
